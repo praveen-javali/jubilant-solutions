d3df5973f7e7ea2126db457b938d2b5567edbb49
pjavali <pjavali@gmail.com>
2019-09-21 21:40:41 +0530

    fix
    fix find buton , contact form, mailphp and images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+function clearall() {
+  document.getElementById('name').value = ' ';
+  document.getElementById('email').value = ' ';
+  document.getElementById('contactNumber').value = ' ';
+  document.getElementById('comments').value = ' ';
+  console.log('cleared');
+}
